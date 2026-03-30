ddc971f886a04e06eb1788d4d10714993b3728a5
lumumbaian22-stack <lumumbaian22@gmail.com>
2026-03-30 18:54:13 +0300

    Update index.js

--- js/features/classes/index.js
+++ js/features/classes/index.js
@@ -1,4 +1,16 @@
 // js/features/classes/index.js
+
+// Export main class manager
 export { classManager } from './ClassManager.js';
+
+// Export teacher assignment manager
 export { teacherAssignment } from './TeacherAssignment.js';
+
+// Export student enrollment manager
 export { studentEnrollment } from './StudentEnrollment.js';
+
+// Export subject assignment manager (if you create a separate file)
+// export { subjectAssignment } from './SubjectAssignment.js';
+
+// Also export the class manager as default for convenience
+export default { classManager, teacherAssignment, studentEnrollment };
